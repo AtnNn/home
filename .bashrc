8b54cef53f1e477da8a3144257fdd8cd8a923fc9
Etienne Laurin <etienne@atnnn.com>
2013-04-27 21:17:33 +0000

    Emacs and bash settings

---
+++ .bashrc
@@ -0,0 +1,29 @@
+#### Sourced by all interactive bash shells
+
+if [[ $- != *i* ]]; then
+    # non-interactive shell
+    return
+fi
+
+. ~/.session/environment
+. ~/.session/lib
+
+### Bash settings
+HISTCONTROL=ignoredups
+HISTFILESIZE=10000
+shopt -s autocd globstar histappend
+
+### Aliases
+alias edit="emacsclient -a '' -n -c"
+alias gl="git log --graph"
+LOCAL_TZ='America/Los_Angeles'
+alias localdate='TZ=$LOCAL_TZ date'
+alias mv='mv -i'
+alias cp='cp -i'
+alias rm='rm -i'
+alias less='less -RS'
+alias make='make --jobs=6 --load-average=4'
+alias gl='git log --graph --oneline --decorate'
+
+### Path
+add_path ~/bin
---
+++ .emacs.d/init.el
@@ -0,0 +1,45 @@
+
+;;; Load saved settings
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;;; Emacs look
+(set-default-font "DejaVu Sans-10")
+
+;;; Change input defaults
+(global-set-key "\M-/" 'hippie-expand)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;; Encodings
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;;; Indentation
+(setq c-basic-offset 4)
+
+;;; Whitespace
+(require 'whitespace)
+(setq whitespace-style '(trailing lines-tail tabs space-before-tab face))
+(setq whitespace-action '(report-on-bogus))
+(setq whitespace-line-column 89)
+(setq require-final-newline 'visit)
+(global-whitespace-mode t)
+
+;;; Buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward)
+(setq uniquify-seperator "\\")
+
+;;; Haskell
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;;; Packages
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/")
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
