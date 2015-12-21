a9d37434fdc99077faa9f0e29a276cee181bf6a1
Etienne Laurin <etienne@atnnn.com>
2015-12-21 00:11:00 +0000

    use sysgit on treetop

---
+++ home/atnnn/.emacs.d/custom.el
@@ -0,0 +1,36 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
+ '(battery-mode-line-format "[%b%p%%]")
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(default-input-method "rfc1345")
+ '(desktop-save-mode t)
+ '(display-battery-mode t)
+ '(fringe-mode (quote (2 . 6)) nil (fringe))
+ '(global-font-lock-mode t)
+ '(ido-mode t nil (ido))
+ '(indent-tabs-mode nil)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(mouse-autoselect-window t)
+ '(save-place t nil (saveplace))
+ '(savehist-mode t)
+ '(scroll-bar-mode nil)
+ '(scroll-preserve-screen-position t)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
+ '(use-file-dialog nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "gainsboro" :foreground "black" :inverse-video t))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "gray" :foreground "grey20" :box (:line-width -1 :color "grey75") :weight light))))
+ '(whitespace-line ((t (:foreground "tomato")))))
---
+++ home/atnnn/.xmonad/xmonad.hs
@@ -0,0 +1,105 @@
+import XMonad
+import Data.Monoid
+import System.Exit
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+myTerminal      = "xterm"
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+myBorderWidth   = 1
+
+myModMask       = mod4Mask
+
+myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+
+myNormalBorderColor  = "#000000"
+myFocusedBorderColor = "#008800"
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm, xK_p), spawn "dmenu_run")
+    , ((modm .|. shiftMask, xK_c), kill)
+    , ((modm, xK_space ), sendMessage NextLayout)
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm, xK_n), refresh)
+
+    , ((modm, xK_Tab), windows W.focusDown)
+    , ((modm, xK_j), windows W.focusDown)
+    , ((modm, xK_k), windows W.focusUp)
+    , ((modm, xK_m), windows W.focusMaster)
+    , ((modm, xK_Return), windows W.swapMaster)
+    , ((modm .|. shiftMask, xK_j), windows W.swapDown)
+    , ((modm .|. shiftMask, xK_k), windows W.swapUp)
+
+    , ((modm, xK_h), sendMessage Shrink)
+    , ((modm, xK_l), sendMessage Expand)
+    , ((modm, xK_t), withFocused $ windows . W.sink)
+    , ((modm, xK_comma), sendMessage (IncMasterN 1))
+    , ((modm, xK_period), sendMessage (IncMasterN (-1)))
+
+    , ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+    , ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart")
+    ]
+    ++
+    [((m .|. modm, k), windows $ f i)
+    | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm, button1), (\w ->
+                          focus w >> mouseMoveWindow w
+                          >> windows W.shiftMaster))
+    , ((modm, button2), (\w ->
+                          focus w >> windows W.shiftMaster))
+    , ((modm, button3), (\w ->
+                          focus w >> mouseResizeWindow w
+                          >> windows W.shiftMaster))
+    ]
+
+myLayout = Full ||| tiled ||| Mirror tiled
+  where
+     tiled   = Tall nmaster delta ratio
+     nmaster = 1
+     ratio   = 1/2
+     delta   = 3/100
+
+myManageHook = composeAll []
+
+myEventHook = mempty
+
+myLogHook = return ()
+
+myStartupHook = return ()
+
+main = xmonad defaults
+
+defaults = defaultConfig {
+      -- simple stuff
+        terminal = myTerminal,
+        focusFollowsMouse = myFocusFollowsMouse,
+        borderWidth = myBorderWidth,
+        modMask = myModMask,
+        workspaces = myWorkspaces,
+        normalBorderColor = myNormalBorderColor,
+        focusedBorderColor = myFocusedBorderColor,
+
+      -- key bindings
+        keys = myKeys,
+        mouseBindings = myMouseBindings,
+
+      -- hooks, layouts
+        layoutHook = myLayout,
+        manageHook = myManageHook,
+        handleEventHook = myEventHook,
+        logHook = myLogHook,
+        startupHook = myStartupHook
+    }
