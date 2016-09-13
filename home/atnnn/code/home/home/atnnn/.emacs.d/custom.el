be3a8e10653a5334e102882ac522466b5a643080
Etienne Laurin <etienne@atnnn.com>
2016-09-13 17:23:25 -0400

    emacs config

---
+++ home/atnnn/code/home/home/atnnn/.emacs.d/custom.el
@@ -0,0 +1,43 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(Info-additional-directory-list (quote ("/home/atnnn/code/ciao/build/doc/")))
+ '(Info-default-directory-list (quote ("/usr/share/info/emacs-24/" "/usr/share/info/")))
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
+ '(battery-mode-line-format "[%b%p%%]")
+ '(blink-cursor-mode nil)
+ '(c-auto-align-backslashes nil)
+ '(column-number-mode t)
+ '(default-input-method "rfc1345")
+ '(desktop-save-mode t)
+ '(display-battery-mode t)
+ '(fringe-mode (quote (2 . 6)) nil (fringe))
+ '(global-font-lock-mode t)
+ '(hippie-expand-try-functions-list
+   (quote
+    (try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill try-complete-lisp-symbol-partially try-complete-lisp-symbol)))
+ '(ido-mode t nil (ido))
+ '(indent-tabs-mode nil)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(mouse-autoselect-window t)
+ '(save-place t nil (saveplace))
+ '(savehist-mode t)
+ '(scroll-bar-mode nil)
+ '(scroll-preserve-screen-position t)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
+ '(use-file-dialog nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "gainsboro" :foreground "black" :inverse-video t))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "gray" :foreground "grey20" :box (:line-width -1 :color "grey75") :weight light))))
+ '(whitespace-line ((t (:foreground "tomato"))))
+ '(whitespace-trailing ((t (:background "red1" :foreground "yellow")))))
---
+++ home/atnnn/code/home/home/atnnn/.emacs.d/init.el
@@ -0,0 +1,59 @@
+
+;;; Load saved settings
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; Emacs look
+(setq default-frame-alist
+      '((font . "DejaVu Sans-10")))
+
+(setq-default scroll-bar-mode 0)
+
+;;; Change input defaults
+(global-set-key "\M-/" 'hippie-expand)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;; Encodings
+(setq-default locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;;; Indentation
+(setq c-basic-offset 4)
+
+;;; Whitespace
+(require 'whitespace)
+(setq-default whitespace-style '(trailing tabs space-before-tab face))
+(setq-default whitespace-action '(report-on-bogus))
+;;(setq-default whitespace-line-column 89)
+(global-whitespace-mode t)
+
+;;; Buffer names
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward)
+(setq uniquify-seperator "\\")
+
+;;; Haskell
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;;; Packages
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/")
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+;; (load-file (let ((coding-system-for-read 'utf-8))
+;;                 (shell-command-to-string "agda-mode locate")))
+
+(add-to-list 'load-path "~/.emacs.d/idris-mode")
+(load "idris-mode")
+(setenv "PATH" (concat "/home/atnnn/.cabal/bin:" (getenv "PATH")))
+(setq exec-path (append exec-path '("/home/atnnn/.cabal/bin")))
+
+;; Paredit
+(add-to-list 'paredit-space-for-delimiter-predicates
+             (lambda (_ _) nil))
