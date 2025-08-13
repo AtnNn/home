eedc885b6d057bf6fb3aa9677302f97ab95b3fb8
Etienne Laurin <etienne@atnnn.com>
2025-08-13 02:37:52 -0700

    fix nebula config bugs

--- shared/default.nix
+++ shared/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 
 {
-  nebula-ca = pkgs.concatTextFiles {
+  nebula-ca = pkgs.concatTextFile {
     name = "nebula-all-ca.crt";
-    files = [ ./nebula-ca.crt ./nebula-ca-old.crt ];
+    files = [ ./nebula-ca.crt ./nebula-ca-previous.crt ];
   };
   garage_rpc_port = 3901;
 }
