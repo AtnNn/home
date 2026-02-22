09b787da0bd07bd676ddb8b30360c750516a15ef
Etienne Laurin <etienne@atnnn.com>
2026-02-21 19:25:36 -0700

    add on-demand

--- home/atnnn/.config/home-manager/home.nix
+++ home/atnnn/.config/home-manager/home.nix
@@ -1,10 +1,45 @@
 { pkgs, config, ... }:
 let
   nix-path = builtins.getEnv "NIX_PATH";
+
   home-manager-custom = pkgs.writeScriptBin "home-manager" ''
     #!${pkgs.stdenv.shell}
     export NIX_PATH="${nix-path}"
     exec ${(import <home-manager> { inherit pkgs; }).home-manager}/bin/home-manager "$@"
+  '';
+
+  on-demand = pkg:
+    if pkg ? meta && pkg.meta ? mainProgram
+    then on-demand-bins pkg [ pkg.meta.mainProgram ]
+    else builtins.trace "warning: package '${pkg.name}' has no mainProgram"
+      (on-demand-bins pkg [ ]);
+
+  on-demand-bins = { name, drvPath, pname ? name, ... }: bins: pkgs.runCommand "on-demand-${name}" {
+    inherit bins name pname;
+    drv = builtins.unsafeDiscardOutputDependency drvPath;
+  } ''
+    mkdir -p $out/bin
+    for bin in $bins; do
+    echo "#!/bin/sh
+    path=\$(nix-store --realise $drv --add-root ~/.local/state/on-demand/$name | head -n 1)
+    if [[ ! -e \$path/bin/$bin ]]; then
+      echo \"error: on-demand: package $name has no binary named '\$bin'\" >&2
+      ls \$path/bin >&2
+      exit 1
+    fi
+    exec \$path/bin/$bin "\$@"
+    " > $out/bin/$bin
+    chmod a+x $out/bin/$bin
+    done
+    echo "
+    #!/bin/sh
+    path=\$(nix-store --realise $drv --add-root ~/.local/state/on-demand/$pname | head -n 1)
+    if [[ \$# = 0 ]]
+    then exec ls \$path
+    else cmd=\$1; shift; exec \$path/bin/\$cmd "$@"
+    fi
+    " > $out/bin/nod-$pname
+    chmod a+x $out/bin/nod-$pname
   '';
 in {
   home = {
@@ -14,6 +49,8 @@ in {
     sessionVariables = {
       NIX_PATH = nix-path;
       EDITOR = "${pkgs.vim}/bin/vim";
+      WLR_RENDER_NO_EXPLICIT_SYNC = "1";
+      WLR_SCENE_DISABLE_DIRECT_SCANOUT = "1";
     };
     sessionPath = [ "~/.local/bin" ];
   };
@@ -40,10 +77,252 @@ in {
     enable = true;
   };
 
-  programs.emacs = {
+  programs.git = {
     enable = true;
-    package = pkgs.emacs;
-    extraPackages = epkg: [
+    # aliases = {};
+    # attributes = [];
+    settings = {
+      push.default = "upstream";
+      rerere.enabled = true;
+      branch.autosetupmerge = false;
+      merge.conflictstyle = "diff3";
+      mergetool.emacs.cmd = ''
+        emacs -nw -Q --no-desktop --eval '(ediff-merge-files-with-ancestor \"'$LOCAL'\" \"'$REMOTE'\" \"'$BASE'\" nil \"'$MERGED'\")'
+      '';
+      log.abbrevCommit = true;
+      log.decorate = "short";
+      format.pretty = "oneline";
+      color.ui = "auto";
+      user.email = "etienne@atnnn.com";
+      user.name = "Etienne Laurin";
+    };
+    ignores = [
+      "*~"
+      "*#"
+      "result"
+      "TAGS"
+    ];
+  };
+
+  home.file = {
+    ".config/home-manager/home.nix.current".source = ./home.nix;
+    ".config/nixpkgs/config.nix".text = ''
+      builtins.fromJSON ${pkgs.lib.strings.escapeNixString (builtins.toJSON config.nixpkgs.config)}
+    '';
+    ".config/nix/nix.conf".text = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+    settings = [{
+      position = "top";
+      layer = "top";
+      modules-left = ["sway/workspaces"];
+      modules-center = ["sway/window" "sway/mode"];
+      modules-right = ["tray" "bluetooth" "pulseaudio" "battery" "clock"];
+      battery = {
+        format = "{capacity}% {time}";
+        format-time = "{H}:{M}";
+      };
+      tray = {
+        show-passive-items = true;
+      };
+      pulseaudio = {
+        on-click = "pavucontrol";
+      };
+    }];
+  };
+  wayland.windowManager.sway = {
+    enable = true;
+    systemd.enable = true;
+    extraConfig = ''
+      mode "kill" {
+        bindsym --whole-window button1 kill;mode default
+        bindsym Escape mode default
+      }
+    '';
+    config = {
+      modifier = "Mod4";
+      input."type:keyboard".xkb_options = "ctrl:nocaps";
+      input."type:touchpad" = {
+        accel_profile = "adaptive";
+        natural_scroll = "enabled";
+        tap = "enabled";
+        drag = "enabled";
+        dwt = "enabled";
+        tap_button_map = "lrm";
+      };
+      keybindings = let
+        modifier = config.wayland.windowManager.sway.config.modifier;
+      in pkgs.lib.mkOptionDefault {
+        "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
+        "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
+        "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
+        "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+        "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
+        "XF86MonBrightnessUp" = "exec brightnessctl set +5%";
+        "XF86AudioPlay" = "exec playerctl play-pause";
+        "XF86AudioNext" = "exec playerctl next";
+        "XF86AudioPrev" = "exec playerctl previous";
+        "${modifier}+s" = ''
+          exec grim -g "`slurp`" -t png - | wl-copy -t image/png
+        '';
+        "${modifier}+l" = ''
+          exec swaylock
+        '';
+        "${modifier}+Shift+q" = "mode kill";
+        "${modifier}+g" = "sticky toggle";
+      };
+      startup = [
+        { command = ''
+            swayidle -w \
+            timeout 600 'swaylock -f -c 000000' \
+            timeout 660 'swaymsg "output * dpms off"' \
+                    resume 'swaymsg "output * dpms on"' \
+                    before-sleep 'swaylock -f -c 000000'
+          '';
+        }
+      ];
+      terminal = "${pkgs.alacritty}/bin/alacritty";
+      # terminal = "alacritty";
+      window.hideEdgeBorders = "both";
+      bars = [];
+      bindswitches = let laptop = "eDP-1"; in {
+        "lid:on" = {
+	        reload = true;
+	        locked = true;
+          action = "output ${laptop} disable";
+        };
+	      "lid:off" = {
+	        reload = true;
+          locked = true;
+          action = "output ${laptop} enable";
+        };
+      };
+    };
+    extraSessionCommands = ''
+    export SDL_VIDEODRIVER=wayland
+    export _JAVA_AWT_WM_NONREPARENTING=1
+    export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+    export QT_QPA_PLATFORM=wayland
+    export MOZ_ENABLE_WAYLAND=1
+    export XDG_SESSION_TYPE=wayland
+    export XDG_CURRENT_DESKTOP=sway
+    '';
+    wrapperFeatures.gtk = true;
+  };
+
+  home.keyboard.options = [ "ctrl:nocaps" ];
+
+  home.packages = [
+    home-manager-custom
+    # nixGL
+    pkgs.autoconf
+    pkgs.automake
+    pkgs.binutils
+    (on-demand pkgs.cmake)
+    pkgs.gnugrep
+    pkgs.gnumake
+    pkgs.graphviz
+    pkgs.iftop
+    pkgs.imagemagick
+    pkgs.jq
+    pkgs.less
+    pkgs.lsof
+    pkgs.netcat-gnu
+    pkgs.nix
+    pkgs.p7zip
+    pkgs.patchelf
+    pkgs.pinentry-curses
+    pkgs.proot
+    (on-demand pkgs.python3)
+    pkgs.rlwrap
+    pkgs.whois
+    pkgs.acpi
+    pkgs.screen
+    pkgs.firefox
+    pkgs.file
+    pkgs.brightnessctl
+    pkgs.playerctl
+    pkgs.pavucontrol
+    pkgs.pkg-config
+    pkgs.grim
+    pkgs.slurp
+    pkgs.elan
+    pkgs.inotify-tools
+    pkgs.htop
+    pkgs.alacritty
+    pkgs.wl-clipboard
+    (on-demand pkgs.blueman)
+    pkgs.ncdu
+    pkgs.nix-diff
+    pkgs.unzip
+    (on-demand pkgs.pnpm)
+    (on-demand pkgs.rustup)
+    (on-demand pkgs.jujutsu)
+    (on-demand pkgs.ffmpeg)
+    (on-demand pkgs.bc)
+    pkgs.coreutils-full
+    (on-demand pkgs.radicle-node)
+    (on-demand pkgs.radicle-tui)
+    (on-demand pkgs.jjui)
+    (on-demand pkgs.zed-editor)
+    (on-demand pkgs.cabal-install)
+    (on-demand-bins pkgs.cppcheck ["cppcheck" "on-demand-test"])
+    (on-demand pkgs.exercism)
+    (on-demand-bins pkgs.gcc ["gcc" "g++"])
+    (on-demand pkgs.clang)
+    (on-demand-bins pkgs.clang-tools ["clang-format" "clang-tidy"])
+    (on-demand pkgs.haskellPackages.git-annex)
+    (on-demand pkgs.gnupg)
+    (on-demand pkgs.go)
+    (on-demand pkgs.isabelle)
+    (on-demand pkgs.ninja)
+    (on-demand pkgs.nodejs)
+    (on-demand pkgs.pandoc)
+    (on-demand pkgs.qemu)
+    (on-demand pkgs.rtorrent)
+    (on-demand pkgs.sbt)
+    (on-demand pkgs.scala)
+    (on-demand pkgs.sqlite)
+    (on-demand pkgs.swi-prolog)
+    (on-demand pkgs.tigervnc)
+    (on-demand-bins pkgs.texlive.combined.scheme-full ["latex" "pdflatex"])
+    (on-demand-bins pkgs.haskellPackages.warp ["warp"])
+    (on-demand pkgs.gdb)
+    (on-demand-bins pkgs.gprolog ["gprolog"])
+    (on-demand pkgs.wineWow64Packages.full)
+    (on-demand pkgs.wasm-pack)
+    (on-demand pkgs.cargo-generate)
+    (on-demand pkgs.signal-desktop)
+    (on-demand pkgs.spirv-tools)
+    (on-demand pkgs.zathura)
+    (on-demand pkgs.jdk)
+    (on-demand (pkgs.lib.hiPrio pkgs.gambit))
+    (on-demand pkgs.pstree)
+    (on-demand pkgs.emscripten)
+    (on-demand pkgs.zathura)
+    (on-demand pkgs.shellcheck)
+    (on-demand pkgs.typescript)
+    (on-demand pkgs.typescript-language-server)
+    (on-demand pkgs.qbittorrent)
+    (on-demand pkgs.i2p)
+    (on-demand pkgs.gh)
+    (on-demand pkgs.lorri)
+    (on-demand pkgs.swift)
+    (on-demand pkgs.mp4v2)
+    (on-demand pkgs.asunder)
+    (on-demand pkgs.magic-wormhole)
+    (on-demand pkgs.oniux)
+    (on-demand pkgs.android-studio-tools)
+    (on-demand pkgs.android-studio)
+    (on-demand pkgs.gemini-cli)
+    (on-demand pkgs.webkitgtk_4_1)
+    (on-demand pkgs.librsvg)
+    (on-demand-bins (pkgs.emacsPackages.withPackages (epkg: [
       epkg.flycheck
       epkg.magit
       epkg.helm
@@ -77,244 +356,50 @@ in {
       epkg.hide-mode-line
       epkg.flycheck-projectile
       epkg.helm-company
-    ];
-  };
-
-  programs.git = {
-    enable = true;
-    # aliases = {};
-    # attributes = [];
-    extraConfig = {
-      push.default = "upstream";
-      rerere.enabled = true;
-      branch.autosetupmerge = false;
-      merge.conflictstyle = "diff3";
-      mergetool.emacs.cmd = ''
-        emacs -nw -Q --no-desktop --eval '(ediff-merge-files-with-ancestor \"'$LOCAL'\" \"'$REMOTE'\" \"'$BASE'\" nil \"'$MERGED'\")'
-      '';
-      log.abbrevCommit = true;
-      log.decorate = "short";
-      format.pretty = "oneline";
-      color.ui = "auto";
-    };
-    ignores = [
-      "*~"
-      "*#"
-      "result"
-      "TAGS"
-    ];
-    userEmail = "etienne@atnnn.com";
-    userName = "Etienne Laurin";
-  };
-
-  home.file = {
-    ".config/home-manager/home.nix.current".source = ./home.nix;
-    ".config/nixpkgs/config.nix".text = ''
-      builtins.fromJSON ${pkgs.lib.strings.escapeNixString (builtins.toJSON config.nixpkgs.config)}
-    '';
-    ".config/nix/nix.conf".text = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  programs.waybar = {
-    enable = true;
-    settings = [{
-      position = "top";
-      layer = "top";
-      modules-left = ["sway/workspaces"];
-      modules-center = ["sway/window" "sway/mode"];
-      modules-right = ["tray" "bluetooth" "pulseaudio" "battery" "clock"];
-      battery = {
-        format = "{capacity}% {time}";
-        format-time = "{H}:{M}";
-      };
-      tray = {
-        show-passive-items = true;
-      };
-      pulseaudio = {
-        on-click = "pavucontrol";
-      };
-    }];
-  };
-  wayland.windowManager.sway = {
-    enable = true;
-    config = {
-      modifier = "Mod4";
-      input."type:keyboard".xkb_options = "ctrl:nocaps";
-      input."type:touchpad" = {
-        accel_profile = "adaptive";
-        natural_scroll = "enabled";
-        tap = "enabled";
-        drag = "enabled";
-        dwt = "enabled";
-        tap_button_map = "lrm";
-      };
-      keybindings = let
-        modifier = config.wayland.windowManager.sway.config.modifier;
-      in pkgs.lib.mkOptionDefault {
-        "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-        "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
-        "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-        "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
-        "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
-        "XF86MonBrightnessUp" = "exec brightnessctl set +5%";
-        "XF86AudioPlay" = "exec playerctl play-pause";
-        "XF86AudioNext" = "exec playerctl next";
-        "XF86AudioPrev" = "exec playerctl previous";
-      };
-      startup = [
-        { command = ''
-            swayidle -w \
-            timeout 600 'swaylock -f -c 000000' \
-            timeout 660 'swaymsg "output * dpms off"' \
-                    resume 'swaymsg "output * dpms on"' \
-                    before-sleep 'swaylock -f -c 000000'
-          '';
-        }
-      ];
-      terminal = "${pkgs.alacritty}/bin/alacritty";
-      # terminal = "alacritty";
-      window.hideEdgeBorders = "both";
-      bars = [{
-        position = "top";
-        command = "waybar";
-      }];
-    };
-    extraSessionCommands = ''
-    export SDL_VIDEODRIVER=wayland
-    export _JAVA_AWT_WM_NONREPARENTING=1
-    export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-    export QT_QPA_PLATFORM=wayland
-    export MOZ_ENABLE_WAYLAND=1
-    export XDG_SESSION_TYPE=wayland
-    export XDG_CURRENT_DESKTOP=sway
-    '';
-    wrapperFeatures.gtk = true;
-  };
-
-  home.keyboard.options = [ "ctrl:nocaps" ];
-
-  home.packages = [
-    home-manager-custom
-    # nixGL
-    pkgs.autoconf
-    pkgs.automake
-    (pkgs.lib.hiPrio pkgs.coreutils)
-    pkgs.binutils
-    # pkgs.cabal-install
-    pkgs.cmake
-    # pkgs.cppcheck
-    # pkgs.exercism
-    (pkgs.lib.hiPrio pkgs.gcc)
-    pkgs.clang
-    pkgs.clang-tools
-    pkgs.haskellPackages.git-annex
-    pkgs.gnugrep
-    pkgs.gnumake
-    pkgs.gnupg
-    # pkgs.go
-    pkgs.graphviz
-    pkgs.iftop
-    pkgs.imagemagick
-    # pkgs.isabelle
-    pkgs.jq
-    # pkgs.lean
-    pkgs.less
-    pkgs.lsof
-    pkgs.netcat-gnu
-    pkgs.ninja
-    # (pkgs.lib.hiPrio pkgs.nixUnstable)
-    pkgs.nix
-    # pkgs.nodejs
-    pkgs.p7zip
-    pkgs.pandoc
-    pkgs.patchelf
-    pkgs.pinentry
-    pkgs.proot
-    pkgs.python3
-    pkgs.qemu
-    pkgs.rlwrap
-    pkgs.rtorrent
-    pkgs.sbt
-    pkgs.scala
-    pkgs.sqlite
-    pkgs.swi-prolog
-    pkgs.tigervnc
-    # pkgs.texlive.combined.scheme-full
-    # pkgs.haskellPackages.warp
-    pkgs.whois
-    pkgs.xterm
-    pkgs.gdb
-    pkgs.gprolog
-    # pkgs.wineWowPackages.full
-    pkgs.wasm-pack
-    # pkgs.cargo-generate
-    pkgs.nodePackages.npm
-    # (pkgs.rust-bin.stable.latest.default.override {
-    #   targets = [
-    #     "x86_64-unknown-linux-gnu"
-    #     "wasm32-unknown-unknown"
-    #   ];
-    # })
-    pkgs.llvmPackages_latest.llvm
-    pkgs.llvmPackages_latest.bintools
-    pkgs.rustup
-    pkgs.llvmPackages_latest.lld
-    pkgs.acpi
-    pkgs.signal-desktop
-    pkgs.screen
-    pkgs.firefox-wayland
-    pkgs.file
-    pkgs.brightnessctl
-    pkgs.playerctl
-    pkgs.pavucontrol
-    pkgs.pkg-config
-    # pkgs.spirv-tools
-    # pkgs.rust-analyzer
-    # pkgs.zathura -- build broken
-    pkgs.grim
-    pkgs.slurp
-    pkgs.jdk
-    pkgs.elan
-    # (pkgs.lib.hiPrio pkgs.gambit)
-    # pkgs.python3Packages.mathlibtools
-    pkgs.inotify-tools
-    pkgs.htop
-    pkgs.alacritty
-    pkgs.wl-clipboard
-    pkgs.pstree
-    # pkgs.emscripten
-    # pkgs.unity3d
-    pkgs.blueman
-    pkgs.zathura
-    pkgs.ncdu
-    pkgs.nix-diff
-    pkgs.unzip
-    pkgs.shellcheck
-    # pkgs.typescript
-    # pkgs.typescript-language-server
-    pkgs.pnpm
-    pkgs.rustup
-    pkgs.qbittorrent
-    pkgs.i2p
-    pkgs.gh
-    pkgs.lorri
-    pkgs.swift
-    pkgs.jujutsu
-    pkgs.ffmpeg
-    pkgs.mp4v2
-    pkgs.asunder
-    pkgs.bc
-    pkgs.coreutils-full
-    pkgs.magic-wormhole
-    pkgs.oniux
-    pkgs.android-studio-tools
-    pkgs.android-studio
-    pkgs.gemini-cli
-    pkgs.webkitgtk_4_1
-    pkgs.librsvg
-    pkgs.radicle-node
-    pkgs.radicle-tui
+    ])) ["emacs" "emacsclient"])
   ];
+
+  programs.rclone = {
+    enable = true;
+  };
+
+  services.blueman-applet.enable = true;
+
+  services.network-manager-applet.enable = true;
+
+  dconf.enable = true;
+
+  services.mako = {
+    enable = true;
+    settings = {
+      default-timeout = 10000;
+    };
+  };
+
+
+  # programs.obs-studio = {
+  #   enable = true;
+  #   plugins = with pkgs.obs-studio-plugins; [
+  #     wlrobs
+  #     waveform
+  #     obs-retro-effects
+  #     obs-freeze-filter
+  #     obs-vintage-filter
+  #   ];
+  # };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-gtk
+        fcitx5-m17n
+        fcitx5-table-other
+        fcitx5-table-extra
+        qt6Packages.fcitx5-unikey
+      ];
+    };
+  };
 }
--- home/atnnn/.config/nixpkgs/upgrade.nix
+++ home/atnnn/.config/nixpkgs/upgrade.nix
@@ -1,19 +1,31 @@
 let
   remotes = {
-    nixpkgs = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
-    home-manager = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-    rust-overlay = "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+    nixpkgs.url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs.patches = [
+      /home/atnnn/.config/nixpkgs/patches/491704.patch
+    ];
+    home-manager.url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+    rust-overlay.url = "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
   };
 
-  nix-path-remotes = pkgs.linkFarm "nix-path" (map (name: { name = name; path = fetchTarball remotes.${name}; }) (builtins.attrNames remotes));
-  pkgs = import (fetchTarball remotes.nixpkgs) { overlays = [];};
-  home-manager = (import (fetchTarball remotes.home-manager) {inherit pkgs;}).home-manager;
+  nix-path-remotes = pkgs.linkFarm "nix-path" (builtins.attrValues nix-path);
+  nix-path = (builtins.mapAttrs (name: { url, patches ? [] }: {
+    name = name;
+    path =
+      let src = fetchTarball url; in
+      if patches != []
+      then pkgsBoot.applyPatches { inherit name src patches; }
+      else src;
+  }) remotes);
+  pkgsBoot = import (fetchTarball remotes.nixpkgs.url) { overlays = [];};
+  pkgs = import nix-path.nixpkgs.path { overlays = [];};
+  home-manager = (import nix-path.home-manager.path {inherit pkgs;}).home-manager;
 
-  nix-path = "${nix-path-remotes}:/nix/var/nix/profiles/per-user/root/channels";
+  nix-path-string = "${nix-path-remotes}";
 
 in
 pkgs.writeScriptBin "home-upgrade" ''
   #!${pkgs.stdenv.shell}
-  export NIX_PATH="${nix-path}"
+  export NIX_PATH="${nix-path-string}"
   exec ${home-manager}/bin/home-manager "${"$"}{@:-switch}"
 ''
